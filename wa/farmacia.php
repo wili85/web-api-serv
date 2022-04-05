71fe8b63df34057eea4ed1d07c418b6563b7c637
Willian <wyamunaque@saludpol.gob.pe>
2022-04-05 11:37:34 -0500

    sp cita..

--- wa/farmacia.php
+++ wa/farmacia.php
@@ -1,5 +1,8 @@
 <?php
 	header('Content-type: application/json');
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+	header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 	
     include_once 'api.php';
     
