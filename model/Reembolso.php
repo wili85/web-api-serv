f2101f3b32643aa3a8afe671c9338380619c24a1
Frank Huatuco <apoyo_efetic7@saludpol.gob.pe>
2023-10-24 12:55:24 -0500

    agregar cci email telf

--- model/Reembolso.php
+++ model/Reembolso.php
@@ -45,9 +45,9 @@ class Reembolso {
 		return $this->readFunctionPostgres('sp_consult_reembolso_xdni_xht',$p);
 		}
 	
-		public function registrarRecetaValeTemporal($p){
-			return $this->readFunctionPostgres('sp_crud_receta_vale_tmp',$p);
-			}
+	public function registrarRecetaValeTemporal($p){
+		return $this->readFunctionPostgres('sp_crud_receta_vale_tmp',$p);
+	}
 
 	public function crudComprobante($p) {
         return $this->readFunctionPostgres('sp_crud_comprobante_new',$p);
