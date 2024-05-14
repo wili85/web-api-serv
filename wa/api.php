8a1b0ed0e6528b0cae8b195fa63851d25a984aff
Willian <wyamunaque@saludpol.gob.pe>
2024-05-14 14:32:51 -0500

    act serv...

--- wa/api.php
+++ wa/api.php
@@ -3632,7 +3632,8 @@ class Api{
 					$prestacion[$i]['codigo'] = $rs[$i]['c_estado_prestacion'];
 					$prestacion[$i]['estado'] = $rs[$i]['v_descripcion'];
 					//$prestacion[$i]['fecha_estado'] = ($rs[$i]['t_fecha_estado_prestacion']!=null)?$rs[$i]['t_fecha_estado_prestacion']:"";
-					$fecha_estado= ($rs[$i]['t_fecha_estado_prestacion']!=null)?$rs[$i]['t_fecha_estado_prestacion']:"";
+					$fecha_estado = ($rs[$i]['t_fecha_estado_prestacion']!=null)?$rs[$i]['t_fecha_estado_prestacion']:"";
+					$prestacion[$i]['fecha_estado'] = $fecha_estado;
 					
 					if ($nr2 > 0) {
 						for ($i2 = 0; $i2 < $nr2; $i2++) {
